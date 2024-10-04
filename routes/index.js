4bb13b7affef672b5656d8e3f2f05e3dcec35932
shanmukh <prajwalshanmukh261@gmail.com>
2024-10-04 16:24:46 +0530

    signupandlogin

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('home');
 });
-
+/*user and admin login page*/
+router.get('/user',function(req,res,next){
+  res.render('usersignup');
+})
+router.get('/login',function(req,res,next){
+  res.render('login')
+})
 module.exports = router;
 
